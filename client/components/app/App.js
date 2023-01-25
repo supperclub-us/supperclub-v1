e54938bfdcf1e11f3d4a7a2ae2b274bde3860f9a
richardyoungdev <richardyoungdev@gmail.com>
2023-01-25 15:45:24 -0500

    Refactored folder names. Created components and store folder; and extermined 3 bugs by using destructure method.

--- client/components/app/App.js
+++ client/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Navbar from '../features/navbar/Navbar';
+import {Navbar} from '../index';
 import AppRoutes from './AppRoutes';
 
 const App = () => {
--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+// import logout from store
+import { logout } from '../auth/authSlice';
+
+
+// import { logout } from '../';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
