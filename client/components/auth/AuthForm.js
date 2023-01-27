22eb5d36a58564ff5cdea997986e3a71f4d49dd2
richardyoungdev <richardyoungdev@gmail.com>
2023-01-27 18:35:19 -0500

    Working on both sign up and log in modals

--- client/components/auth/AuthForm.js
+++ client/components/auth/AuthForm.js
@@ -124,6 +124,24 @@ const AuthForm = ({ name, displayName }) => {
 export default AuthForm;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, { useState } from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
 // import { authenticate } from '../../store/store';
