75b50cb69c5ef40fb4b78126461f2edd559d4926
Joe Holder <jholder090@yahoo.com>
2023-01-27 16:03:46 -0700

    captured location to local state on search bar

--- client/components/searchBar/SearchBar.js
+++ client/components/searchBar/SearchBar.js
@@ -12,33 +12,42 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import styled from "styled-components";
 import useInput from "./useInput";
+import MapboxAccessToken from "../../env";
 import "./searchBar.css"
 
 const SearchBar = () => {
   const [numGuests, setNumGuests] = useState();
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
+  const [location, setLocation] = useState();
 
-
-  const handleChange = (e) => {
+  const handleGuests = (e) => {
     setNumGuests(e.target.value);
   };
 
-  const handleDistance = (e) => {
-    setDistance(e.target.value);
-  };
+  // const handleDistance = (e) => {
+  //   setDistance(e.target.value);
+  // };
+
 
   const address = useInput("");
-  console.log("ADDRESS", address)
+
+  const handleClick = () => {
+    setLocation(address.value)
+    console.log("LOCATION", location)
+    // console.log("GUESTS", numGuests)
+    // console.log("START DATE", startDate)
+    // console.log("END DATE", endDate)
+  }
 
   return (
     // will switch box to formControl
     <Box
       className="search-bar"
       variant="contained"
-      sx={{ p: 2, border: "1px solid grey"}}
+      sx={{ p: 2, border: "1px solid grey" }}
     >
-      <FormControl className="form-control" sx={{display: "flex", justifyContent: "flex-start", alignItems: "flex-start", m: "1em"}}>
+      <FormControl className="form-control" sx={{ display: "flex", justifyContent: "flex-start", alignItems: "flex-start", m: "1em" }}>
         <TextField
           id="outlined-basic"
           label="Location"
@@ -46,43 +55,44 @@ const SearchBar = () => {
           {...address}
         />
         {address.suggestions?.length > 0 && (
-        <Box className="searchBar-suggestionWrapper" sx={{ bgcolor: '#FCFCFC', position:'absolute', width: 400, mt: 7, mb: 0, mx: 0, borderRadius: '5px', boxShadow: 3}} >
-          {address.suggestions.map((suggestion, index) => {
-            return (
-              <Box className="searchBar-suggestion" sx={{maxWidth: 400, p: 0.4, "&:hover": {color: 'blue' }}} key={index}
-              onClick={() => {
-                address.setValue(suggestion.place_name);
-                address.setSuggestions([]);
-              }}>
-                {suggestion.place_name}
-              </Box>
-            )
-          })}
-        </Box>
+          <Box className="searchBar-suggestionWrapper" sx={{ bgcolor: '#FCFCFC', position: 'absolute', width: 400, mt: 7, mb: 0, mx: 0, borderRadius: '5px', boxShadow: 3 }} >
+            {address.suggestions.map((suggestion, index) => {
+              return (
+                <Box className="searchBar-suggestion" sx={{ maxWidth: 400, p: 0.4, "&:hover": { color: 'blue' } }} key={index}
+                  onClick={() => {
+                    address.setValue(suggestion.place_name);
+                    address.setSuggestions([]);
+                  }}>
+                  {suggestion.place_name}
+                </Box>
+              )
+            })}
+          </Box>
         )}
       </FormControl>
 
-      <FormControl className="form-control" sx={{m: "1em"}}>
+      <FormControl className="form-control" sx={{ m: "1em" }}>
         <InputLabel id="demo-simple-select-label">
           Guests
         </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="guestNumber-picker"
-            value={numGuests}
-            label="Guests"
-            onChange={handleChange}
-            sx={{ width: "25ch" }}
-            placeholder="Guests"
-          >
-            <MenuItem value={10}>1</MenuItem>
-            <MenuItem value={20}>2</MenuItem>
-            <MenuItem value={30}>3</MenuItem>
-          </Select>
+        <Select
+          defaultValue={""}
+          labelId="demo-simple-select-label"
+          id="guestNumber-picker"
+          value={numGuests}
+          label="Guests"
+          onChange={handleGuests}
+          sx={{ width: "25ch" }}
+          placeholder="Guests"
+        >
+          <MenuItem value={10}>1</MenuItem>
+          <MenuItem value={20}>2</MenuItem>
+          <MenuItem value={30}>3</MenuItem>
+        </Select>
 
       </FormControl>
 
-      <FormControl className="form-control" sx={{m: "1em"}}>
+      <FormControl className="form-control" sx={{ m: "1em" }}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             label="Start Date"
@@ -95,7 +105,7 @@ const SearchBar = () => {
         </LocalizationProvider>
       </FormControl>
 
-      <FormControl className="form-control" sx={{m: "1em"}}>
+      <FormControl className="form-control" sx={{ m: "1em" }}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             label="End Date"
@@ -107,6 +117,7 @@ const SearchBar = () => {
           />
         </LocalizationProvider>
       </FormControl>
+      <button onClick={handleClick}></button>
     </Box>
   );
 };
