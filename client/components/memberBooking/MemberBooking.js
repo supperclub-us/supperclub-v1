0938758bfcb1323288c8f4e8df97eeb96fafe0df
aguilana <naguila.mandich@gmail.com>
2023-02-02 11:06:44 -0500

    member booking page start

--- client/components/memberBooking/MemberBooking.js
+++ client/components/memberBooking/MemberBooking.js
@@ -1,30 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchSingleBookingAsync, selectSingleChefBookings } from '../slices/singleBookingSlice';
-import { LinearProgress } from '@mui/material';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import {
+  fetchSingleBookingAsync,
+  selectSingleChefBookings,
+} from "../slices/singleBookingSlice";
+import { LinearProgress } from "@mui/material";
 
 const MemberBooking = () => {
   const dispatch = useDispatch();
   const { booking, error, isLoading } = useSelector(selectSingleChefBookings);
-  console.log("BOOKING FOR MEMBER", booking)
+  console.log("BOOKING FOR MEMBER", booking);
   const { bookingId } = useParams();
 
   useEffect(() => {
     dispatch(fetchSingleBookingAsync(bookingId));
   }, []);
 
-if (isLoading) {
-  return <LinearProgress />
-}
-if(error) {
-  return <div>{error}</div>
-}
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
   return (
     <div>
-      {booking.title}
+      <div className="memberBooking-title-images">
+        <div className="memberBooking-title">
+          <h1>{booking.title}</h1>
+          <p>
+            <small>Hosted by <Link to={`/users/chefs/${booking.chefId}`} style={{color: "red"}}>{booking?.chefBooking?.firstName}</Link></small>
+          </p>
+        </div>
+        <div className="memberBooking-images">{}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MemberBooking
+export default MemberBooking;
