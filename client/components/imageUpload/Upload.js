d74bb48d747ec637f5076a9984bd6c5ab639ed6d
Joe Holder <jholder090@yahoo.com>
2023-02-08 16:13:13 -0700

    added prevent default to image upload form

--- client/components/imageUpload/Upload.js
+++ client/components/imageUpload/Upload.js
@@ -9,17 +9,17 @@
 //     // const formData = new Formdata()
 //     setImage(URL.createObjectURL(event.target.files[0]))
 //   };
-  
+
 //   const handleImageUpload = (data) => {
 //     setImage(data.secure_url);
 //   }
-  
+
 //   return (
 //     <CloudinaryContext cloudName="dm8eizfpl">
 //       <div>
 //         <input type="file" onChange={handleUpload}/>
 //         {image && (
-//           <ImageUpload 
+//           <ImageUpload
 //             publicId={image}
 //             onSuccess={handleImageUpload}
 //             cloudName="dm8eizfpl"
@@ -48,30 +48,31 @@ import { Image } from 'cloudinary-react'
 const Upload = () => {
   const [selectedImage, setSelectedImage] = useState("");
 
-  const handleUpload = () => {
-   const formData = new FormData();
-   formData.append("file", selectedImage)
-   formData.append("upload_preset", "jeux3vde")
-   
-   Axios.post(
-    "https://api.cloudinary.com/v1_1/dm8eizfpl/image/upload", formData
-   )
-   
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", selectedImage)
+    formData.append("upload_preset", "jeux3vde")
+
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/dm8eizfpl/image/upload", formData
+    )
+
   };
-  
+
   return (
     <div>
-      <input 
-      type="file" 
-      onChange={(event) => setSelectedImage(event.target.files[0])} 
+      <input
+        type="file"
+        onChange={(event) => setSelectedImage(event.target.files[0])}
       />
       <button onClick={handleUpload}> Upload </button>
-      
-      <Image 
-      cloudName="dm8eizfpl"
+
+      <Image
+        cloudName="dm8eizfpl"
       // publicId={}
       />
-      
+
     </div>
   )
 }
