ba079bfab7bd60479e927846f120404a492d7efb
aguilana <naguila.mandich@gmail.com>
2023-02-09 10:53:45 -0500

    added stripe folder in components along with 2 files - payment and confirmation component

---
+++ client/components/stripePayment/Confirmation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Confirmation = () => {
+  return (
+    <div> THANK YOU! 🥳 </div>
+  )
+}
+
+export default Confirmation
---
+++ client/components/stripePayment/Payment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div>Payment Page</div>
+  )
+}
+
+export default Payment
