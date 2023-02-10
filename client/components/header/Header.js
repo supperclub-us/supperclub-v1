9358269d71c3928b5021918bf0020228545d5827
Joe Holder <jholder090@yahoo.com>
2023-02-10 15:34:20 -0700

    merge with Nick v2

--- client/components/header/Header.js
+++ client/components/header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { SearchBar } from '../index'
-
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const user = useSelector((state) => state.auth.me);
 
   return (
     <div>
