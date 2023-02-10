bb04018ef7909f126e44f0ab79ca52aabb80f2a1
Joe Holder <jholder090@yahoo.com>
2023-02-10 10:34:56 -0700

    login no longer navigates to map

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -31,7 +31,6 @@ const Navbar = () => {
     setModalScreen("");
 
     user.role === "CHEF" ? navigate(`/users/chefprofile/${user.id}`) : null
-    user.role === "MEMBER" ? navigate(`/map`) : null
   };
 
   const renderModalScreen = () => {
