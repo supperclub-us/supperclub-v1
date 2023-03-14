b860b28e67ae859a35fb5f47912c6c8f9a2ed96e
aguilana <naguila.mandich@gmail.com>
2023-03-14 16:32:13 -0400

    footer text altered

--- client/components/footer/Footer.js
+++ client/components/footer/Footer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import React from 'react';
+import { Facebook, Instagram, Twitter } from '@mui/icons-material';
 import {
   FaCcAmex,
   FaCcDiscover,
@@ -7,35 +7,35 @@ import {
   FaCcStripe,
   FaCcVisa,
   FaPaypal,
-} from "react-icons/fa";
-import { Typography } from "@mui/material";
-import "./footer.css";
+} from 'react-icons/fa';
+import { Typography } from '@mui/material';
+import './footer.css';
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-row">
-        <div className="footer-column">
-          <Typography variant="body2">Where to Waze Us </Typography>
-          <Typography variant="body2">123 Team G FullStack Street</Typography>
-          <Typography variant="body2">New York, NY, USA</Typography>
-          <Typography variant="body2">Tel: 718-123-4567</Typography>
+    <footer className='footer-container'>
+      <div className='footer-row'>
+        <div className='footer-column'>
+          <Typography variant='body2'>Where to Find Us </Typography>
+          <Typography variant='body2'>123 10th Ave </Typography>
+          <Typography variant='body2'>New York, NY, USA</Typography>
+          <Typography variant='body2'>Tel: 718-123-4567</Typography>
         </div>
-        <div className="footer-column">
-          <Typography variant="body2"> Contact Us </Typography>
-          <Typography variant="body2"> About Us </Typography>
-          <Typography variant="body2"> FAQ </Typography>
-          <Typography variant="body2"> Terms of Service </Typography>
+        <div className='footer-column'>
+          <Typography variant='body2'> Contact Us </Typography>
+          <Typography variant='body2'> About Us </Typography>
+          <Typography variant='body2'> FAQ </Typography>
+          <Typography variant='body2'> Terms of Service </Typography>
         </div>
-        <div className="footer-column">
-          <Typography variant="body2">Follow us!</Typography>
-          <Twitter fontSize="medium" />
-          <Instagram fontSize="medium" />
-          <Facebook fontSize="medium" />
+        <div className='footer-column'>
+          <Typography variant='body2'>Follow us!</Typography>
+          <Twitter fontSize='medium' />
+          <Instagram fontSize='medium' />
+          <Facebook fontSize='medium' />
         </div>
       </div>
-      <div className="footer-row-payment">
-        <div className="footer-row-cards">
+      <div className='footer-row-payment'>
+        <div className='footer-row-cards'>
           <FaPaypal />
           <FaCcVisa />
           <FaCcDiscover />
@@ -43,8 +43,8 @@ function Footer() {
           <FaCcMastercard />
           <FaCcStripe />
         </div>
-        <Typography variant="body2">
-          © 2023 Supper Club | We are best turtles at FullStack
+        <Typography variant='body2'>
+          © 2023 Supper Club | All Rights Reserved
         </Typography>
       </div>
     </footer>
